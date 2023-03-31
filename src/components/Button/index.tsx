ad845a9a3a9fa13e908a7628b7d28925746ae730
Riley1101 <www.riley.s.eileen@gmail.com>
2023-03-30 22:10:36 -0400

    Add Cypress tests workflow and update Button component styles and Tailwind config

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,3 @@
 export function Button() {
-  return <button>Click Me</button>;
+  return <button className="p-4 text-2xl text-white text-blue-500 border rounded-md">Click Me</button>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,31 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors :{
+        "theme-accent":"#377DFF",
+        "theme-secondary":"#4E5D78",
+
+        "theme-foreground-light":"#FFFFFF",
+        "theme-foreground-dark":"#191C21",
+
+        "theme-background-light":"#F7F8FA",
+        "theme-background-dark":"#212833",
+
+        "theme-text-light":"#727E93",
+        "theme-text-dark":"#D3D4D8",
+
+        "theme-muted-light":"#959EAE",
+        "theme-muted-dark":"#848B9A",
+      },
+      borderRadius:{
+        "theme":"20px",
+      }
+    },
   },
   plugins: [],
 }
