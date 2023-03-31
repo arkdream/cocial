55bb06151e477e38211d558bba9e62d6afc72a4d
Riley1101 <www.riley.s.eileen@gmail.com>
2023-03-30 22:25:36 -0400

    Fix some of the type error and test error

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+const AboutPage = (props: Props) => {
+  return (
+    <div className='p-4'>
+        <h1 className='text-2xl'>About Page</h1>
+    </div>
+  )
+}
+export default AboutPage;
